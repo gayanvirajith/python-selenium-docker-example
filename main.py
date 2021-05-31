be41179bb5095c6565c95252984a3c0ed1ad58f7
Apple <Apple@MacBook-Pro.local>
2021-05-31 15:56:34 +0530

    add main method

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-gpu')
 
-driver = webdriver.Chrome(options=chrome_options)
-driver.implicitly_wait(3)
-url = "https://scrapeme.live/shop/"
-driver.get(url)
-#elements = driver.find_elements_by_xpath("//div[@class='expeties-block']/div/a/h4")
-elements = driver.find_elements_by_class_name('woocommerce-loop-product__title')
+def main():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
 
-expertise = []
-for element in elements:
-    expertise.append(element.get_attribute('innerHTML'))
-print(r'Defined expertise in %s' % url)
-print(expertise)
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.implicitly_wait(3)
+    url = "https://scrapeme.live/shop/"
+    driver.get(url)
+    #elements = driver.find_elements_by_xpath("//div[@class='expeties-block']/div/a/h4")
+    elements = driver.find_elements_by_class_name('woocommerce-loop-product__title')
+
+    expertise = []
+    for element in elements:
+        expertise.append(element.get_attribute('innerHTML'))
+    print(r'Defined expertise in %s' % url)
+    print(expertise)
+
+if __name__ == '__main__':
+    main()
